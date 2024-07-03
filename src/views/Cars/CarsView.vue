1af271a2e177e7792810e4add12e46b1cb51d871
radu cruceat <radu.cruceat@gmail.com>
2024-07-03 21:18:31 +0300

    added cars table

--- src/views/Cars/CarsView.vue
+++ src/views/Cars/CarsView.vue
@@ -1,7 +1,22 @@
 
 <template>
     <main>
-        <h1>Car Page</h1>
+        <h1  style="text-align:center">Car Page</h1>
+
+        <table class="table table-bordered" style="width:80%; margin: 0 auto;">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Marca</th>
+                    <th>Model</th>
+                    <th>An</th>
+                    <th>Motor</th>
+                </tr>
+
+            </thead>
+
+
+        </table>
 
     </main>
 </template>
